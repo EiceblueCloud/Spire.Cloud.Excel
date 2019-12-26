98215006f6d3d158ebd82014823deb6b5b2e80fe
E-iceblue Product Family <7018422+eiceblue@users.noreply.github.com>
2019-12-26 16:35:46 +0800

    Create convertToPdf.py

---
+++ Python Excel Examples/convert/put_workbook_convert/convertToPdf.py
@@ -0,0 +1,12 @@
+import spirecloudexcel
+from spirecloudexcel.configuration import Configuration as ExcelConfiguration
+
+appId = "your id"
+appKey = "your key"
+configuration = ExcelConfiguration(appId, appKey)
+api = spirecloudexcel.api.general_api.GeneralApi(configuration)
+
+file = "D:/data/test.xlsx"
+password = None
+outputPath = "output/result.pdf"
+result = api.put_workbook_convert("Pdf", outputPath, workbook=file, password=password)
